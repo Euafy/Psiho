ea0434bf4f92d2e1589b30ab2f8ec4fdc8301cbd
Euafy <ioncosmin682@gmail.com>
2022-04-12 23:59:56 +0300

    gata

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 var counter = 1;
-
+var imgCount = 1;
 var r = 255;
 var g = 0; 
 var b = 0;
@@ -9,7 +9,6 @@ function curcubeu() {
     if (r === 255 && g < 255 && b === 0) {
         g++;
         document.getElementById("vesela").style.color = "rgb(" + r + ", " + g + ", " + b +")"
-        console.log(g)
     }
 
     if (r > 0 && g === 255 && b === 0) {
@@ -41,8 +40,6 @@ function hocusPocus() {
     while (x==y) {
         y = Math.floor(Math.random() * 30 + -10)
     }
-    console.log(x);
-    console.log(y);
     document.getElementById("titlu").innerHTML = "Da click pe curcubeu!";
     document.getElementById("counter").innerHTML = "Click-uri: " + counter;
     document.getElementById("vesela").innerHTML = "Bunaaa :)"
@@ -57,7 +54,63 @@ function hocusPocus() {
 
 
 function party() {
+
     counter++
+    imgCount++
+    if (imgCount == 120) imgCount = 10
+    if (imgCount === 10) {
+        document.getElementById("10").style.display = "flex"
+        document.getElementById("110").style.display = "none"
+    }
+    if (imgCount === 20) {
+        document.getElementById("10").style.display = "none"
+        document.getElementById("20").style.display = "flex"
+    }
+    if (imgCount === 30) {
+
+        document.getElementById("20").style.display = "none"
+        document.getElementById("30").style.display = "flex"
+    }
+    if (imgCount === 40) {
+
+        document.getElementById("30").style.display = "none"
+        document.getElementById("40").style.display = "flex"
+    }
+    if (imgCount === 50) {
+
+        document.getElementById("40").style.display = "none"
+        document.getElementById("50").style.display = "flex"
+    }
+    if (imgCount === 60) {
+
+        document.getElementById("50").style.display = "none"
+        document.getElementById("60").style.display = "flex"
+    }
+    if (imgCount === 70) {
+
+        document.getElementById("60").style.display = "none"
+        document.getElementById("70").style.display = "flex"
+    }
+    if (imgCount === 80) {
+
+        document.getElementById("70").style.display = "none"
+        document.getElementById("80").style.display = "flex"
+    }
+    if (imgCount === 90) {
+
+        document.getElementById("80").style.display = "none"
+        document.getElementById("90").style.display = "flex"
+    }
+    if (imgCount === 100) {
+
+        document.getElementById("90").style.display = "none"
+        document.getElementById("100").style.display = "flex"
+    }
+    if (imgCount === 110) {
+
+        document.getElementById("100").style.display = "none"
+        document.getElementById("110").style.display = "flex"
+    }
     var x = Math.floor(Math.random() * 35 + -35)
     var y = Math.floor(Math.random() * 30 + -10)
     while (x==y) {
